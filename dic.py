daa1b9e3d088e152249a9da2af7d5818c7489dcd
Tomek <tomek.zietkiewicz@gmail.com>
2011-06-06 12:13:37 +0200

    DIC works, not very fast, gives diffrent reults than apriori, but, it seems, these are correct reults For example: for mushroom set with support = 0.8 apriori doesn't finad itemset (34, 36, 85, 86) which is large (checked with regexp directly in data file) and which is found by DIC

--- dic.py
+++ dic.py
@@ -1,12 +1,13 @@
 # -*- coding: utf-8 -*-
 
-from trie import node, root
+from trie import Root
 
 class Dic:
     def __init__(self, transactions, minsup, M):
         self.__minsup = minsup
         self.__transactions = transactions
         self.__M = M
+        self.__minsup_count = self.get_minsup_count()
 
     def get_large_sets(self):
         """
@@ -14,15 +15,35 @@ class Dic:
         """
         large_sets = {}
         
-        root = Root()
+        minsup_count = self.get_minsup_count()
+        print minsup_count
+        self.root = Root(minsup_count)
         finished = False
-        
-        i = 0
+        pass_counter = 0
         while not finished:
-            i = (i + 1) % len(self.transactions)
-            position = i / M
-            finished = root.increment(self.transactions[i], position)
-            if i % M == 0:
-                root.tree.update_child_states()
-        large_sets = root.get_large_sets()
+            print "pass %d" % (pass_counter)
+            for i, transaction in enumerate(self.__transactions):
+                position = i / self.__M
+                if i % self.__M == 0:
+                    #print "\tpart %d" % (position)
+                    finished = self.root.update_child_states(position)                    
+                    if finished and (position > 0 or pass_counter > 0):
+                        break
+                self.root.increment(transaction, position)
+            pass_counter += 1
+
+        large_sets = self.root.get_large_sets(large_sets)
         return large_sets
+        
+    def get_minsup_count(self):
+        """
+        Calculates and returns miniumum support given in number of transactions.
+        """
+        return int(len(self.__transactions) * self.__minsup)
+
+'''
+from transactions import TransactionsList
+transactions = TransactionsList("data\mushroom.dat")
+dic = Dic(transactions, 0.8, 500)
+#dic.get_large_sets()
+'''
