6637a9a5e0711001e2d0e88274b7cf827b46458d
Krzysztof Urban <krz.urb@gmail.com>
2011-06-09 12:32:39 +0200

    Fixed timings in 'data_collect.py'.

--- collect_data.py
+++ collect_data.py
@@ -6,21 +6,23 @@ from dic import Dic
 from stats import Stats
 from rules import RulesGenerator
 
-infile = 'data/accidents.dat'
+infile = 'data/kosarak.dat'
 
 algorithms = ['apriori']
-supports = reversed([0.05, 0.1, 0.15, 0.2, 0.25, 0.3, 0.35, 0.4, 0.45, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0])
+supports = reversed([0.05, 0.15, 0.25, 0.35])
 confidencies = [0.7]
-ms = [100, 1000, 10000]
+ms = [1000, 5000, 25000]
 
 last_total_time = 0
 last_user_time = 0
 
-def process(transactions, algorithm_name, support, confidence, m):
-    global last_total_time
-    global last_user_time
-    global infile
-    filename = infile
+def process(infile, algorithm_name, support, confidence, m):
+    stats = Stats()
+    transactions = TransactionsList(infile)
+    stats.record_post_large_sets()
+    stats.record_post_rules()
+    last_total_time = stats.real_time
+    last_user_time = stats.user_time
     stats = Stats()
     if algorithm_name == 'apriori':
         algorithm = Apriori(transactions, support)
@@ -39,16 +41,15 @@ def process(transactions, algorithm_name, support, confidence, m):
     memory = stats.memory_use
     rules_no = len(rules)
 
-    print "{algorithm_name}\t{filename}\t{support}\t{confidence}\t{m}\t{rules_no}\t{large_len}\t{memory}\t{total_time}\t{user_time}\t{large_sets_time}".format(**locals())
+    print "{algorithm_name}\t{infile}\t{support}\t{confidence}\t{m}\t{rules_no}\t{large_len}\t{memory}\t{total_time}\t{user_time}\t{large_sets_time}".format(**locals())
 
 if __name__ == '__main__':
-    transactions = TransactionsList(infile)
     print "Alg_name\tdataset\tsupp\tconf\tm\trl_no\tlrg_len\tmem\ttot_t\tusr_t\tset_t"
     for algorithm in algorithms:
         for support in supports:
             for confidence in confidencies:
                 if algorithm == 'dic':
                     for m in ms:
-                        process(transactions, algorithm, support, confidence, m)
+                        process(infile, algorithm, support, confidence, m)
                 else:
-                    process(transactions, algorithm, support, confidence, "n/a")
+                    process(infile, algorithm, support, confidence, "n/a")
