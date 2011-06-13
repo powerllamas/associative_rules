a8ef7435c2999399ef5e03203a13753bd045a012
Tomek Ziętkiewicz <tomek.zietkiewicz@gmail.com>
2011-06-13 04:03:07 +0200

    DIC works, sometimes faster than apriori

--- collect_data.py
+++ collect_data.py
@@ -6,14 +6,14 @@ from dic import Dic
 from stats import Stats
 from rules import RulesGenerator
 
-infile = 'data/mushroom.dat'
+infiles = ['data/mushroom.dat', 'data/kosarak.dat', 'data/accidents.dat']
 
-algorithms = ['apriori', 'dic']
-supports = [0.9]
+algorithms = ['dic', 'apriori']
+supports = [0.9]#, 0.8, 0.7, 0.6, 0.5, 0.4, 0.35, 0.3, 0.25]
 confidencies = [0.8]
-ms = [1000]
+ms = [1250]#, 250, 500, 1000, 2000, 8124]
 
-def process(transactions, algorithm_name, support, confidence, m):
+def process(transactions, algorithm_name, support, confidence, m, infile):
     stats = Stats()
     if algorithm_name == 'apriori':
         algorithm = Apriori(transactions, support)
@@ -28,17 +28,22 @@ def process(transactions, algorithm_name, support, confidence, m):
     large_sets_time = stats.set_gen_time
     memory = stats.memory_use
     rules_no = len(rules)
+    ls_size = 0
+    for size in large_sets.values():
+      ls_size += len(size)
 
-    print "{algorithm_name};{support};{confidence};{m};{total_time};{user_time};{large_sets_time};{memory};{rules_no};".format(**locals())
+    print "{infile};{algorithm_name};{support};{confidence};{m};{total_time};{user_time};{large_sets_time};{memory};{rules_no};{ls_size}".format(**locals())
 
 if __name__ == '__main__':
-    transactions = TransactionsList(infile)
-    print "Alg_name;supp;conf;m;tot_t;usr_t;set_t;mem;rl_no;"
-    for algorithm in algorithms:
-        for support in supports:
-            for confidence in confidencies:
-                if algorithm == 'dic':
-                    for m in ms:
-                        process(transactions, algorithm, support, confidence, m)
-                else:
-                    process(transactions, algorithm, support, confidence, "n/a")
+
+    print "File;Alg_name;supp;conf;m;tot_t;usr_t;set_t;mem;rl_no;ls_no"
+    for infile in infiles:
+      transactions = TransactionsList(infile)
+      for algorithm in algorithms:
+	  for support in supports:
+	      for confidence in confidencies:
+		  if algorithm == 'dic':
+		      for m in ms:
+			  process(transactions, algorithm, support, confidence, m, infile)
+		  else:
+		      process(transactions, algorithm, support, confidence, "n/a", infile)
--- dic.py
+++ dic.py
@@ -1,49 +1,83 @@
 # -*- coding: utf-8 -*-
 
 from trie import Root
+from collections import defaultdict
+import math, time
 
 class Dic(object):
     def __init__(self, transactions, minsup, M):
         self.__minsup = minsup
         self.__transactions = transactions
         self.__M = M
-        self.__minsup_count = self.get_minsup_count()
+        self.__minsup_count = self.__get_minsup_count()
 
     def get_large_sets(self):
         """
         Calculates large sets from transactions. Returns dictionary where the key is arity and the value is list of item sets.
         """
         large_sets = {}
+        counter = self.__count_items_in_transactions(self.__transactions)
+        minsup_count = self.__get_minsup_count()
+        L1 = self.__getL1(counter, minsup_count)
+
+        self.root = Root(self.__minsup, len(self.__transactions), L1)
+        self.root.update_states(0, 0)
         
-        minsup_count = self.get_minsup_count()
-        self.root = Root(minsup_count)
+        parts_no = math.ceil(float(len(self.__transactions)) / float(self.__M))
         finished = False
-        pass_counter = 0
+        pass_counter = 1
         while not finished:
+#            print "pass: %d" % (pass_counter)
             for i, transaction in enumerate(self.__transactions):
                 position = i / self.__M
-                if i % self.__M == 0:
-                    #print "\tpart %d" % (position)
-                    finished = self.root.update_child_states(position)                    
-                    if finished and (position > 0 or pass_counter > 0):
-                        break
                 self.root.increment(transaction, position)
+                if (i+1) % self.__M == 0 or (i+1) == len(self.__transactions):
+                    #print "\tpart %d" % (position + 1)
+                    trans_in_last_part = (i+1) - position * self.__M
+                    next_position = (position + 1)% parts_no
+                    finished = self.root.update_states(next_position, trans_in_last_part)                    
+                    if finished:
+                        break
             pass_counter += 1
 
+        #self.root.print_node(large_only = True)
+        #print "%d passes" % (pass_counter)
         large_sets = self.root.get_large_sets(large_sets)
         return large_sets
 
     def get_counter(self):
         return DicCounter(self.root)
         
-    def get_minsup_count(self):
+    def get_large_sets_and_counter(self):
+        return self.get_large_sets(), self.get_counter()
+
+    @staticmethod
+    def __count_items_in_transactions(transactions):
+        """
+        Counts items and returns dictionary with them as keys and their count as value.
+        """
+        counter = defaultdict(int)
+        for transaction in transactions:
+            for item in transaction:
+                counter[(item,)] += 1
+        return counter
+
+    def __get_minsup_count(self):
         """
         Calculates and returns miniumum support given in number of transactions.
         """
         return int(len(self.__transactions) * self.__minsup)
 
-    def get_large_sets_and_counter(self):
-        return self.get_large_sets(), self.get_counter()
+    @staticmethod
+    def __getL1(counter, minsup_count):
+        """
+        Calculates and returns first large set.
+        """
+        L1 = []
+        for k, v in counter.iteritems():
+            if v >= minsup_count:
+                L1.append(k)
+        return sorted(L1)    
 
 class DicCounter:
     def __init__(self, root):
@@ -56,8 +90,20 @@ class DicCounter:
         else:
             return node.counter
 
+#__file__ = '__main__'
+
 if __file__ == '__main__':
+
     from transactions import TransactionsList
     transactions = TransactionsList("data\mushroom.dat")
-    dic = Dic(transactions, 0.8, 500)
-    #dic.get_large_sets()
+    dic = Dic(transactions, 0.8, 1000)
+    start = time.clock()
+    ls = dic.get_large_sets()
+    stop = time.clock()
+    elapsed = stop - start
+    count = 0
+    for sets in ls.values():
+        count += len(sets)
+    print "Large sets number: %d" % (count,)
+    print elapsed
+    print ls
